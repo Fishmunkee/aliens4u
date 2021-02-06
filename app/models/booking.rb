c361549850264fcb92df7704e7552bdef19a334f
Tristan Ramilo <tristanxanderjohn@hotmail.com>
2021-02-06 16:54:35 +0000

    Added validations and routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :alien
+  validates :appointment_start_date, presence :true
+  validates :appointment_end_date, presence :true
+  validates :date_posted, presence :true
 end
+
