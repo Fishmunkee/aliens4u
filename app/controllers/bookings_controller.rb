d8f6405023df8e979fe7288fe270c3ede0ffdfa1
Tristan Ramilo <tristanxanderjohn@hotmail.com>
2021-02-09 20:29:41 +0000

    changed nesting to users instead of aliens

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,13 +4,13 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
-  # def create
-  #   @alien = Alien.find(params[:alien_id])
-  #   @booking = Booking.new(booking_params)
-  # end
+  def create
+    @alien = Alien.find(params[:alien_id])
+    @booking = Booking.new(booking_params)
+  end
 
-  # private
-  # def booking_params
-  #   params.require(:booking).permit()
-  # end
+  private
+  def booking_params
+    params.require(:booking).permit(:appointment_start_date, :appointment_end_date, :date_posted)
+  end
 end
