d937e72bde45432fd3c4312e458ae297a48d4baf
Oliver Norgate <olli.norgate@hotmail.co.uk>
2021-02-13 12:46:11 +0000

    added faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts 'Seeding user'
-@user = User.find("1")
-if @user.nil?
-  @user = User.new
-  @user.email = "test1@test.com"
-  @user.password = "fohdsobnfdaivn"
-  @user.save!
+
+# @user = User.find(1)
+# if @user.nil?
+#   @user = User.new
+#   @user.email = "test1@test.com"
+#   @user.password = "fohdsobnfdaivn"
+#   @user.save!
+# end
+
+
+
+# Alien.create(name: 'Jar Jar Binks', species: 'Gungan', planet: 'Naboo', user: @user)
+# puts 'Finished seeding'
+3.times do
+  puts "Creating users"
+  user = User.create(
+    email: Faker::Internet.email,
+    password: "password"
+    )
+
+    10.times do alien = Alien.create(
+      name: Faker::JapaneseMedia::DragonBall.character,
+      species: Faker::JapaneseMedia::DragonBall.race,
+      planet: Faker::JapaneseMedia::DragonBall.planet,
+      user: user
+      )
+  end
 end
 
-puts 'Seeding aliens'
 
-Alien.create(name: 'Jar Jar Binks', species: 'Gungan', planet: 'Naboo', user: @user)
-puts 'Finished seeding'
